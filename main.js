8d104738a34b0eef93badb640e0b96e02be297f4
davidepatruno <patrunodavid@gmail.com>
2018-11-13 16:22:08 +0100

    calcolo work in progress

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var eta = prompt("inserisci la tua età");
+
+document.getElementById('eta').innerHTML = eta;
+
+var km = prompt("inserisci i chilometri percorsi");
+
+document.getElementById('km').innerHTML = km;
